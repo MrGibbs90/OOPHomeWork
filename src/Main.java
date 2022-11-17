fd2cb15528994f1bd2dd0c70bffd90eea41c6138
MrGibbs90 <mrgibbs9090@mail.ru>
2022-11-17 17:04:11 +0700

    ДЗ-3. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-    }
-    public static void task1() {
+
         Human maksim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
         maksim.hello();
         Human anya = new Human(1993, "Аня", "Москва", "методист образовательных программ");
