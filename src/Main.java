37a63d7628fc8be22c3badd257f34976cddde8aa
MrGibbs90 <mrgibbs9090@mail.ru>
2022-12-20 14:16:31 +0700

    Инкапсуляция. ДЗ-1. Задача-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         Human maksim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
         maksim.hello();
         Human anya = new Human(1993, "Аня", "Москва", "методист образовательных программ");
@@ -11,5 +10,43 @@ public class Main {
         artem.hello();
         Human vladimir = new Human(2001, "Владимир", "Казань", "");
         vladimir.hello();
+
+        Flower rose = new Flower(null, "Голландия", 35.59);
+        Flower chrysanthemum = new Flower(null, null, 15, 5);
+        Flower peony = new Flower(null, "Англия", 69.9, 1);
+        Flower gypsophila = new Flower(null, "Турция", 19.5, 10);
+        printInfo(rose);
+        printInfo(chrysanthemum);
+        printInfo(peony);
+        printInfo(gypsophila);
+        printCoastOfBouquet(
+                rose, rose, rose,
+                chrysanthemum, chrysanthemum, chrysanthemum, chrysanthemum, chrysanthemum,
+                gypsophila
+        );
+    }
+
+    private static void printInfo(Flower flower) {
+        System.out.println(
+                "Цвет: " + flower.getFlowerColor() +
+                        ", страна: " + flower.getCountry() +
+                        ", стоимость за штуку: " + flower.getCost() +
+                        ", срок стояния цветка: " + flower.lifeSpan
+        );
+    }
+
+    private static void printCoastOfBouquet(Flower... flowers) {
+        double totalCost = 0;
+        int minLifeSpan = Integer.MAX_VALUE;
+        for (Flower flower : flowers) {
+            if (flower.lifeSpan < minLifeSpan) {
+                minLifeSpan = flower.lifeSpan;
+            }
+            totalCost += flower.getCost();
+            printInfo(flower);
+        }
+        totalCost = totalCost * 1.1;
+        System.out.println("Стоимость букета: " + totalCost);
+        System.out.println("Срок стояния букета: " + minLifeSpan);
     }
 }
